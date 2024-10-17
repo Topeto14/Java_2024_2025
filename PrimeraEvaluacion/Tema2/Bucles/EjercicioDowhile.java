350436f3c47a5bb80a1d300c2f0ef644a876024e
Topeto14 <levishurtadorobert@gmail.com>
2024-10-17 12:51:23 +0200

    Ejercicios del tema 2 Actualizados

---
+++ PrimeraEvaluacion/Tema2/Bucles/EjercicioDowhile.java
@@ -0,0 +1,34 @@
+package Tema2.Bucles;
+
+import java.util.Scanner;
+
+public class EjercicioDowhile {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //Mismo del password >= 8
+        String password;
+        do {
+            System.out.println("Dime contraseña");
+            password = sc.nextLine();
+        } while (password.length() < 8);
+
+        System.out.println("Tu contraseña es " + password);
+
+        //Muestra los números del 1 al 10 usando do-while
+        int numero = 1;
+        do {
+            System.out.println(numero);
+            numero++;
+        } while (numero < 10);
+
+        numero = 100;
+        do {
+            System.out.println(numero);
+            numero--;
+        } while (numero > 0);
+
+    }
+}
