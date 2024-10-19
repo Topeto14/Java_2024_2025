eb006b19b21c91275d910924ef6cacc321147915
Topeto14 <levishurtadorobert@gmail.com>
2024-10-19 14:21:50 +0200

    Ejercicio 2 y 3 de Repaso

---
+++ PrimeraEvaluacion/Tema2/EjercicioRepaso/Ejercicio3Divisible7ForWhile.java
@@ -0,0 +1,20 @@
+package Tema2.EjercicioRepaso;
+
+public class Ejercicio3Divisible7ForWhile {
+    public static void main(String[] args) {
+        //Muestra todos los números divisibles entre 7 entre  el número 500 y el 1.
+        /*
+        for (int i = 500; i >=1 ; i--) {
+            if (i%7 == 0) {
+                System.out.println(i);
+            }
+        }*/
+        int number = 500;
+        do {
+            if(number%7 ==0){
+                System.out.println(number);
+            }
+            number--;
+        }while (number>=1);
+    }
+}
