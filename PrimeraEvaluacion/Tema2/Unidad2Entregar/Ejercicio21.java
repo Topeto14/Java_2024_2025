634dc5fcd21adbf1a6bcf2427ac6467e297aee36
Topeto14 <levishurtadorobert@gmail.com>
2024-10-24 23:00:15 +0200

    Ejercicio que hemos hecho en clase con metodos try and catch

--- PrimeraEvaluacion/Tema2/Unidad2Entregar/Ejercicio21.java
+++ PrimeraEvaluacion/Tema2/Unidad2Entregar/Ejercicio21.java
@@ -7,13 +7,48 @@ public class Ejercicio21 {
         // 21. Pedir un número entero positivo N. A continuación, introducir por teclado N números enteros.
         // Mostrar la media de los N números introducidos, el mayor y el menor.
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Dime una cantidad e numeros " );
-        int quantity = 0 ;
-        for (int  i=0; i<= quantity; i++){
-            System.out.println(" ");
+        int numerosAIntroducir=0;
+        int numero;
+        int mayor=0;
+        int menor=0;
+        int acumulador=0;
 
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Dime cuántos números tengo que pedir");
+            numerosAIntroducir = Integer.parseInt(sc.nextLine());
+
+            //En un for pedimos todos esos números
+            for(int i=1; i <= numerosAIntroducir; i++) {
+                System.out.println("Dime el número " + i + ": ");
+                numero = Integer.parseInt(sc.nextLine());
+                acumulador += numero;
+
+                //El primer número que me digas es el mayor y el menor
+                if (i == 1) {
+                    mayor = numero;
+                    menor = numero;
+                }
+                if (numero > mayor) {
+                    //Si el número es mayor que el mayor, el mayor ahora es el número
+                    mayor = numero;
+                }
+                if (numero < menor) {
+                    menor = numero;
+                }
+            }
+
+        } catch (NumberFormatException ex) {
+            System.out.println("Número incorrecto");
         }
 
+        System.out.println("El mayor es: " + mayor);
+        System.out.println("El menor es: " + menor);
+        System.out.println("La media es: " + ((double)acumulador / numerosAIntroducir));
+
+
     }
+
 }
+
