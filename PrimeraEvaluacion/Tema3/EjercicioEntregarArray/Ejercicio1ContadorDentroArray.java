6ca6ba90277b4adcc345e53e1252f9bc66c72508
Topeto14 <levishurtadorobert@gmail.com>
2024-11-14 15:33:46 +0100

    Ejercicios ejemplos de Arrays 3

--- PrimeraEvaluacion/Tema3/EjercicioEntregarArray/Ejercicio1ContadorDentroArray.java
+++ PrimeraEvaluacion/Tema3/EjercicioEntregarArray/Ejercicio1ContadorDentroArray.java
@@ -1,6 +1,6 @@
 package Tema3.EjercicioEntregarArray;
 
-public class Ejercicio1 {
+public class Ejercicio1ContadorDentroArray {
     public static int generarNumero(int min, int max){
         return (int) (Math.random()*(max-min+1)+min);
     }
---
+++ PrimeraEvaluacion/Tema3/EjercicioEntregarArray/Ejercicio3.java
@@ -0,0 +1,43 @@
+package Tema3.EjercicioEntregarArray;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static int generarNumero(int min, int max){
+        return (int) (Math.random()*(max-min+1)+min);
+
+    }
+    public static void AcabadoDigito () {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño del array ");
+        int size = Integer.parseInt(sc.nextLine());
+        int Array[] = new int[size];
+        for (int i = 0; i < Array.length; i++) {
+            Array[i] = generarNumero(1, 300);
+            System.out.println(Array[i]);
+
+        }
+        int numeroFinal [] = new int[size];
+
+        for (int i = 0; i < Array.length; i++) {
+            System.out.println(" Dime cual es el último digito que quieres que te enseñe de los numeros que tenemos ");
+            int ultimoDigito = Integer.parseInt(sc.nextLine());
+            if (Array[i] % 10 == ultimoDigito) {
+                numeroFinal[ultimoDigito] = Array[i];
+
+            }
+        }
+        System.out.println(numeroFinal[i]);
+    }
+    public static void main(String[] args) {
+        // Crea un array de números de un tamaño pasado por teclado, el array contendrá números aleatorios entre 1 y 300
+        // y mostrar aquellos números que acaben en un dígito que nosotros le indiquemos por teclado (debes controlar
+        // que se introduce un numero correcto), estos deben guardarse en un nuevo array. Por ejemplo, en un array de 10
+        // posiciones e indicamos mostrar los números acabados en 5, podría salir 155, 25, etc. Cuando llene el segundo
+        // array finalizamos.
+
+        AcabadoDigito();
+
+    }
+}
+
