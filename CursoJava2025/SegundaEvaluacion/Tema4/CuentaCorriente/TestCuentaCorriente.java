3fae301595a7f125611d5ced8be40ade32159d03
Topeto14 <levishurtadorobert@gmail.com>
2025-01-14 19:23:23 +0100

    Orientados Objetos pizza y Tarjeta Regalo

---
+++ CursoJava2025/SegundaEvaluacion/Tema4/CuentaCorriente/TestCuentaCorriente.java
@@ -0,0 +1,31 @@
+package SegundaEvaluacion.Tema4.CuentaCorriente;
+
+public class TestCuentaCorriente {
+    public static void main(String[] args) throws Exception {
+
+        CuentaCorriente cc1 = new CuentaCorriente(1000.0);
+        CuentaCorriente cc2 = new CuentaCorriente(500.0);
+
+        System.out.println(cc1);
+        System.out.println(cc2);
+
+        try {
+            cc2.cargo(1000.0);
+        } catch (Exception e) {
+            System.out.println("Error");
+            System.out.println(e.getMessage());
+        }
+
+        cc2.cargo(1000.0);
+
+        System.out.println("Gracias por usar el cajero");
+
+
+
+
+
+
+
+    }
+
+}
---
+++ CursoJava2025/SegundaEvaluacion/Tema4/Pizza/TestPizza.java
@@ -0,0 +1,24 @@
+package SegundaEvaluacion.Tema4.Pizza;
+
+public class TestPizza {
+    public static void main(String[] args) {
+
+        Pizza pz1 = new Pizza(Pizza.Size.MEDIANA, Pizza.Tipo.BARBACOA);
+        Pizza pz2 = new Pizza(Pizza.Size.FAMILIAR, Pizza.Tipo.CUATROQUESOS);
+
+        System.out.println(pz1);
+        System.out.println(pz2);
+
+        pz1.sirve();
+        pz1.sirve();
+        pz2.sirve();
+
+        System.out.println(pz1);
+        System.out.println(pz2);
+
+
+        System.out.println("Pizzas pedidas: " + Pizza.getTotalPedidas());
+        System.out.println("Pizzas servidas: " + Pizza.getTotalServidas());
+
+    }
+}
