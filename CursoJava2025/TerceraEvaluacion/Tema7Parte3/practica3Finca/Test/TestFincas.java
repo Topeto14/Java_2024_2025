e95b75040cd86e52bef3a22b5fbe05c65d613aef
Topeto14 <levishurtadorobert@gmail.com>
2025-05-07 02:01:03 +0200

    Tema 7 parte 3 practica 3 fincas empezando hacer el test

--- CursoJava2025/TerceraEvaluacion/Tema7Parte3/practica3Finca/Test/TestFincas.java
+++ CursoJava2025/TerceraEvaluacion/Tema7Parte3/practica3Finca/Test/TestFincas.java
@@ -3,6 +3,7 @@ package TerceraEvaluacion.Tema7Parte3.practica3Finca.Test;
 import TerceraEvaluacion.Tema7Parte3.practica3Finca.io.DAOFinca;
 import TerceraEvaluacion.Tema7Parte3.practica3Finca.io.DAOLectura;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 
@@ -25,6 +26,31 @@ public class TestFincas {
         System.out.println("---- 3 Fincas mas grandes -----");
         DAOFinca.getMasGrandes().forEach(System.out::println);
 
+        System.out.println("Fincas por ciudad");
+        DAOFinca.getFincasPorCiudad().forEach((k, v) -> System.out.println(k + ": " + v) );
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Fincas entre 50 y 150 metros cuadrados");
+        DAOFinca.getFincasMedio().forEach(System.out::println);
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Lecturas por finca");
+        DAOLectura.getLectrurasPorFinca().forEach((k, v) -> System.out.println(k + ": " + v) );
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Temperatura máxima finca");
+        System.out.println(DAOLectura.getTempMaximaFinca(30));
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Temperatura mínima finca");
+        System.out.println(DAOLectura.getTempMinimaFinca(2));
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Humedad finca");
+        System.out.println(DAOLectura.getHunedadPorFinca(5));
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Temperatura finca");
+        System.out.println(DAOLectura.getTemperaturaPorFinca(10));
+        System.out.println("----------------------------------------------------------------");
+        /**System.out.println("Temperatura finca en un dia especifico");
+        DAOLectura.getTemperaturaPorFinca(10, LocalDate.of(2023, 7, 20))
+                .forEach(System.out::println);
+        System.out.println("----------------------------------------------------------------");*/
 
 
     }
